3d38355b69020636e373359a973ed38442201d5d
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-10 20:28:13 +0530

    Added:Append

---
+++ Append/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Append
+{
+    internal class Program
+    {
+        private Node head;
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+                next = null;
+            }
+        }
+        public bool Add(int data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return true;
+            }
+            n.next = head;
+            head = n;
+            return true;
+        }
+        public bool Append(int data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return true;
+            }
+
+            Node t = head;
+            while (t.next != null)
+            {
+                t = t.next;
+            }
+            t.next = n;
+            return true;
+        }
+        public static void Main()
+        {
+            LinkedList<int> L = new LinkedList<int>();
+            L.AddFirst(70);
+            L.AddFirst(30);
+            L.AddFirst(56);
+
+            Console.Write("Linked List elements are: ");
+            foreach (int i in L)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
