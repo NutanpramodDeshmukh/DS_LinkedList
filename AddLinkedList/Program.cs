a695ee9c6e5aa0df318fccc74fcc6e3511253a1a
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-10 19:46:33 +0530

    Added: add new list

---
+++ AddLinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using AddLinkedList.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AddLinkedList.Properties.LinkedList;
+
+namespace AddLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> L = new LinkedList<int>();
+            L.AddFirst(70);
+            L.AddFirst(30);
+            L.AddFirst(56);
+
+            Console.Write("Linked List elements are: ");
+            foreach (int i in L)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ AddLinkedList/Properties/LinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddLinkedList.Properties
+{
+    internal class LinkedList
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node head;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+            public bool Add(int data)
+            {
+                Node n = new Node(data);
+                if (head == null)
+                {
+                    head = n;
+                    return true;
+                }
+                n.next = head;
+                head = n;
+                return true;
+            }
+        }
+    }
+}
