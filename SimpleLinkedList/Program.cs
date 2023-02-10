1c4192e8636989094cc82880bbde5a704580155b
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-10 19:08:57 +0530

    Added:new list

---
+++ SimpleLinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = { 56, 30, 70 };
+            Console.WriteLine("Displaying elements of a Linked List: ");
+            LinkedList<int> MyList = new LinkedList<int>(arr);
+            foreach (int res in MyList)
+            {
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
